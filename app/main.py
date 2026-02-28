03c9333ed6a9cb9550d404018460060903d86552
Alejo2236 <alejo.benitez2236@gmail.com>
2026-02-27 20:11:51 -0500

    Create the app settings config.py and minimal use in main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from app.core.logging import configure_logging
-
+from app.core.config import get_settings
 
 configure_logging()
 
-app = FastAPI()
+settings = get_settings()
+
+app = FastAPI(title=settings.app_name)
