a258b9cc43a9a97f595dabc5a68411fe50001ce7
Alejo2236 <alejo.benitez2236@gmail.com>
2026-02-27 20:40:09 -0500

    Integrate the app settings with logging logic, with basic main.py implementation (#4)

--- app/main.py
+++ app/main.py
@@ -2,8 +2,12 @@ from fastapi import FastAPI
 from app.core.logging import configure_logging
 from app.core.config import get_settings
 
-configure_logging()
-
 settings = get_settings()
+
+configure_logging(
+    log_level=settings.log_level,
+    log_to_file=settings.log_to_file,
+    log_file_path=settings.log_file_path,
+)
 
 app = FastAPI(title=settings.app_name)
